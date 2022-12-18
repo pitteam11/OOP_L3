bcf1a34b88594023af9e43116fe3056756b8df36
pitteam11 <117634580+pitteam11@users.noreply.github.com>
2022-12-18 11:03:28 +0300

    Исправлен абонент

--- abonent.h
+++ abonent.h
@@ -7,19 +7,48 @@
 using namespace std;
 
 class Abonent{
-	const char* name;
-	const char* phone;
-/*
-	inline int getSize(char* cstr) {
-		return _msize(cstr) / sizeof(char);
+	char* name{ nullptr };
+	char* phone{ nullptr };
+	
+	inline int getSize(const char* cstr) {
+		return sizeof(cstr) / sizeof(char);
 	}
-*/
+
 public:
-	Abonent() {}
-	Abonent(const char* name, const char* phone) :
-		name{ name },
-		phone{ phone } {}
+	Abonent() {
+	}
+
+	Abonent(const char* name, const char* phone) {
+		int sz1 = strlen(name)+1;
+		int sz2 = strlen(phone)+1;
+		this->name = new char[sz1];
+		this->phone = new char[sz2];
+		
+		for (int k = 0; k < sz1; ++k)
+			this->name[k] = name[k];
+		for (int k = 0; k < sz2; ++k)
+			this->phone[k] = phone[k];
+	}
+
+	Abonent(const Abonent& v) {
+		(*this) = v;		
+	}
+
+	Abonent& operator = (const Abonent& v){
+		int sz1 = strlen(v.name) + 1;
+		int sz2 = strlen(v.phone) + 1;
+		this->name = new char[sz1];
+		this->phone = new char[sz2];		
+		for (int k = 0; k < sz1; ++k)
+			name[k] = v.name[k];
+		for (int k = 0; k < sz2; ++k)
+			phone[k] = v.phone[k];
+		return *this;
+	}
+
 	~Abonent() {		
+		delete[] name;
+		delete[] phone;		
 	}
 	const char* getName() {
 		return name;
@@ -27,28 +56,12 @@ public:
 	const char* getPhone() {
 		return phone;
 	}
-/*
-	char* toCString() {
-		int szName = getSize(name);
-		int szPhone = getSize(phone);
-		char* res = new char[(int)szName + szPhone];		
-		for (int k = 0; k < getSize(name); ++k) {
-			res[k] = name[k];
-		}
-		res[szName-1] = ' ';
-		for (int k = 0; k < szPhone; ++k) {
-			res[k + szName] = phone[k];
-		}
-		return res;
-	}
-*/
 };
 
 class PhoneBook  {
-	Abonent* abonents;
+	Abonent* abonents{nullptr};
 	int size = 0;
 public:
-
 	PhoneBook() {};
 	PhoneBook(const char* filename) {
 		read(filename);
@@ -57,25 +70,15 @@ public:
 	void add(const char* name, const char* phone) {		
 		Abonent abon(name, phone);
 		Abonent* buff = new Abonent[size + 1];
-		for (int k = 0; k < size; ++k)
-			buff[k] = abonents[k];
+		for (int k = 0; k < size; ++k) {
+			buff[k] = abonents[k]; // this is bad copy -- we must reload operator =
+		}
 		buff[size] = abon;
 		delete[] abonents;
 		abonents = buff;
 		size++;
 	}
-
-	void add(string name, string phone) {
-		Abonent abon(name.data(), phone.data());
-		Abonent* buff = new Abonent[size + 1];
-		for (int k = 0; k < size; ++k)
-			buff[k] = abonents[k];
-		buff[size] = abon;
-		delete[] abonents;
-		abonents = buff;
-		size++;
-	}
-
+	
 	void write(const char* filename) {
 		ofstream fout(filename);
 		for (int k = 0; k < size; k++) {
@@ -91,9 +94,9 @@ public:
 			while (!fin.eof()) {
 				string name, phone;
 				fin >> name;
-				fin >> phone;				
-				//add(name.data(), phone.data());
-				cout << name.data() << " " << phone.data() << endl;
+				fin >> phone;
+				if(name.size())
+					add(name.data(), phone.data());
 			}
 			fin.close();
 		}
@@ -106,13 +109,7 @@ public:
 		}
 	}
 
-	~PhoneBook() {
-		/*
-		for (int k = 0; k < size; ++k) {
-			delete[] (abonents[k].getName());
-			delete[] (abonents[k].getPhone());
-		}
-		*/
+	~PhoneBook() {		
 		delete[] abonents;
 	}
 };
